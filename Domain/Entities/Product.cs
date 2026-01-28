048c064071d26a3d82831ca3884a7f8c74f3ee9c
Kwadwo Tuffuor Quayson <kwadwot2002@gmail.com>
2026-01-28 05:16:29 +0000

    Initial commit

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public decimal Pice { get; set; }
+    public decimal Price { get; set; }
+    public string? PictureUrl { get; set; } = string.Empty;
+    public string? Brand { get; set; } = string.Empty;
+    public int StockQuantity { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
